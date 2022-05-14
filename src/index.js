5291cb0745baebaa4924e10c0c4c47688bfc27cb
RaCode75 <racode75@gmail.com>
2022-05-14 08:24:38 -0300

    Engine settings done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //initializations
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //midlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //global variables
+app.use((req, res, next) => {
+    next();
+});
 
 //routes
 app.use(require('./routes'));
