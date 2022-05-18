b24cf5b903c00d95db8f6ad0cac0ab9e65bf3529
RaCode75 <racode75@gmail.com>
2022-05-18 04:12:45 -0300

    Database created

--- database/db.sql
+++ database/db.sql
@@ -3,10 +3,14 @@ CREATE DATABASE db_pantones;
 USE db_pantones;
 
 CREATE TABLE users(
-    PRIMARY KEY id INT(11) NOT NULL AUTO_INCREMENT,
+    id INT(11) NOT NULL,
     username VARCHAR(16) NOT NULL,
     password VARCHAR(20) NOT NULL,
     fullname VARCHAR(100) NOT NULL
 );
 
-DESCRIBE users;
+ALTER TABLE users
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE users   
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
